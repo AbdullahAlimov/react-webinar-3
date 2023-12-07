d217e3b50a70dc0ff141c4b0ce48e4a760ef3452
AbdullahAlimov <ilia.filatov.04@gmail.com>
2023-12-07 21:00:53 +0300

    Удаление лишних импортов

--- src/app.js
+++ src/app.js
@@ -1,15 +1,10 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Router from './components/router';
-import Basket from './components/basket';
-import useSelector from './store/use-selector';
 
 const App = () => {
-    const activeModal = useSelector(state => state.modals.name);
-
     return (
         <div>
             <Router/>
-            {activeModal === 'basket' && <Basket/>}
         </div>
     );
 };
