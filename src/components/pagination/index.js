149da10bfd60c0948f1c3d593a66c8434eb3dbd0
AbdullahAlimov <ilia.filatov.04@gmail.com>
2023-12-11 17:46:07 +0300

    Исправление бага пагинации
    Исправление бага, который возникает, когда length меньше 5

--- src/components/pagination/index.js
+++ src/components/pagination/index.js
@@ -1,27 +1,32 @@
-import React, { memo,useState,useEffect } from 'react';
+import React, { memo, useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import "./style.css"
 
-function Pagination ({length,selectItem,onRefresh}){
+function Pagination({ length, selectItem, onRefresh }) {
 
-    const [list,setList]=useState([])
+    const [list, setList] = useState([])
 
     useEffect(() => {
         var newList = []
-
-        newList.push(1);
-        if (selectItem > 3) newList.push("...");
-        if (selectItem === length) newList.push(selectItem - 2);
-        if (!(selectItem === 1 || selectItem === 2)) newList.push(selectItem - 1);
-        if (!(selectItem === 1 || selectItem === length)) newList.push(selectItem);
-        if (!(selectItem === length || selectItem === length - 1)) newList.push(selectItem + 1);
-        if (selectItem === 1) newList.push(selectItem + 2);
-        if (selectItem < length - 2) newList.push("...");
-        newList.push(length);
-
+        if (length <= 4) {
+            for (let item = 1; item <= length; item++) {
+                newList.push(item)
+            }
+        }
+        else {
+            newList.push(1);
+            if (selectItem > 3) newList.push("...");
+            if (selectItem === length) newList.push(selectItem - 2);
+            if (!(selectItem === 1 || selectItem === 2)) newList.push(selectItem - 1);
+            if (!(selectItem === 1 || selectItem === length)) newList.push(selectItem);
+            if (!(selectItem === length || selectItem === length - 1)) newList.push(selectItem + 1);
+            if (selectItem === 1) newList.push(selectItem + 2);
+            if (selectItem < length - 2) newList.push("...");
+            newList.push(length);
+        }
         setList(newList);
-    }, [selectItem,length]);
-    
+    }, [selectItem, length]);
+
     return (
         <ul className='Pagination'>
             {list?.map((item, index) => {
@@ -49,14 +54,14 @@ function Pagination ({length,selectItem,onRefresh}){
     );
 };
 
-Pagination.PropTypes={
-    list:PropTypes.array,
-    selectItem:PropTypes.number,
-    onRefresh:PropTypes.func
+Pagination.PropTypes = {
+    list: PropTypes.array,
+    selectItem: PropTypes.number,
+    onRefresh: PropTypes.func
 }
 
-Pagination.defaultProps={
-    onRefresh:()=>{}
+Pagination.defaultProps = {
+    onRefresh: () => { }
 }
 
 export default memo(Pagination);
