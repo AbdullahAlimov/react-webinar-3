69e863b9d2e84703228a1de7a62939d00227b8b0
AbdullahAlimov <ilia.filatov.04@gmail.com>
2023-12-14 18:20:11 +0300

    Добавление логики сохранения, хранения, удаления данных пользователя

---
+++ src/store/user/index.js
@@ -0,0 +1,97 @@
+import StoreModule from "../module";
+
+class UserState extends StoreModule {
+    initState() {
+        return {
+            isAutorized: false,
+            userInfo: {
+                name: "",
+                email: "",
+                phone: "",
+            }
+        }
+    }
+    async autorization(login, password) {
+        const url = '/api/v1/users/sign';
+        const data = {
+            login: login,
+            password: password
+        };
+        return new Promise(async (resolve) => {
+            const response = await fetch(url, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(data)
+            })
+            const json = await response.json();
+
+            if (json.error) {
+                resolve(json.error.message)
+            }
+            else {
+                localStorage.setItem("token", json.result.token)
+
+                await this.setState({
+                    isAutorized: true,
+                    userInfo: {
+                        name: json.result.user.username,
+                        email: json.result.user.email,
+                        phone: json.result.user.profile.phone,
+                    }
+                }, "Пользователь успешно вошел")
+                resolve("")
+            }
+        });
+    }
+    exit() {
+        const url = "/api/v1/users/sign";
+        const headers = new Headers({
+            'X-Token': localStorage.getItem("token"),
+            'Content-Type': 'application/json',
+        });
+
+        const fetchData = async () => {
+            await fetch(url, {
+                method: 'DELETE',
+                headers: headers
+            });
+        }
+        fetchData();
+
+        localStorage.removeItem("token")
+        this.setState({
+            ...this.initState()
+        }, "Пользователь вышел")
+    }
+    load() {
+        if (localStorage.getItem("token")) {
+            const url = "/api/v1/users/self?fields=*";
+            const headers = new Headers({
+                'X-Token': localStorage.getItem("token"),
+                'Content-Type': 'application/json',
+            });
+
+            const fetchData = async () => {
+                const response = await fetch(url, {
+                    method: 'GET',
+                    headers: headers
+                });
+                const json = await response.json();
+
+                await this.setState({
+                    isAutorized: true,
+                    userInfo: {
+                        name: json.result.username,
+                        email: json.result.email,
+                        phone: json.result.profile.phone,
+                    }
+                }, "Пользователь успешно вошел")
+            }
+            fetchData();
+        }
+    }
+}
+
+export default UserState;
