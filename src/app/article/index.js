fe0cf717a5f5dca5ff632244c03b855ab5f85a83
AbdullahAlimov <ilia.filatov.04@gmail.com>
2023-12-21 23:41:39 +0300

    Добавление  контейнера сообщений

--- src/app/article/index.js
+++ src/app/article/index.js
@@ -1,4 +1,4 @@
-import {memo, useCallback, useMemo} from 'react';
+import {memo, useCallback} from 'react';
 import {useParams} from 'react-router-dom';
 import useStore from '../../hooks/use-store';
 import useTranslate from '../../hooks/use-translate';
@@ -13,6 +13,8 @@ import TopHead from '../../containers/top-head';
 import {useDispatch, useSelector} from 'react-redux';
 import shallowequal from 'shallowequal';
 import articleActions from '../../store-redux/article/actions';
+import commentsActions from "../../store-redux/comments/actions"
+import Comments from '../../containers/comments';
 
 function Article() {
   const store = useStore();
@@ -25,6 +27,7 @@ function Article() {
   useInit(() => {
     //store.actions.article.load(params.id);
     dispatch(articleActions.load(params.id));
+    dispatch(commeActions.load())
   }, [params.id]);
 
   const select = useSelector(state => ({
@@ -49,6 +52,7 @@ function Article() {
       <Spinner active={select.waiting}>
         <ArticleCard article={select.article} onAdd={callbacks.addToBasket} t={t}/>
       </Spinner>
+      <Comments articleId={params.id}/>
     </PageLayout>
   );
 }
---
+++ src/containers/comments/index.js
@@ -0,0 +1,88 @@
+import React, { memo, useCallback, useMemo, useState } from 'react';
+import CommentList from '../../components/comment-list';
+import AddCommentForm from '../../components/add-comment-form';
+import listToTree from '../../utils/list-to-tree';
+import treeToList from '../../utils/tree-to-list';
+import { useDispatch, useSelector as useReduxSelector } from 'react-redux';
+import useSelector from '../../hooks/use-selector';
+import { useLocation, useNavigate } from 'react-router-dom';
+import commentActions from "../../store-redux/comments/actions"
+import Spinner from '../../components/spinner';
+import PropTypes from 'prop-types';
+
+const Comments = ({articleId}) => {
+
+    const navigate = useNavigate();
+    const location = useLocation();
+    const dispatch = useDispatch();
+
+    const select = useReduxSelector(state => ({
+        comments: state.comments.items,
+        waiting: state.comments.waiting,
+    }))
+    const exists = useSelector(state => state.session.exists)
+
+    const [selectParent, setSelectParent] = useState({
+        _id: articleId,
+        _type: "article",
+    })
+
+    const callbacks = {
+        // Переход к авторизации
+        onSignIn: useCallback(() => {
+            navigate('/login', { state: { back: location.pathname } });
+        }, [location.pathname]),
+
+        changeSelectParent: useCallback((id) => {
+            setSelectParent({
+                _id:id,
+                _type:"comment"
+            })
+        },[selectParent]),
+        resetParent: useCallback(() => {
+            setSelectParent(({
+                _id: articleId,
+                _type: "article",
+            }))
+        }, [articleId]),
+        addComment: useCallback((text) => {
+            dispatch(commentActions.addComment({text,parent:selectParent,id:articleId}))
+            setSelectParent(({
+                _id: articleId,
+                _type: "article",
+            }))
+        }, [selectParent])
+    }
+
+    const lists = {
+        comment: useMemo(() => ((treeToList(listToTree(select.comments), (item, level) => (
+            {
+                id: item._id,
+                author: item.author,
+                date: item.dateCreate,
+                depth: level-1,
+                content: item.text
+            }
+        )))).slice(1),[select.comments]),
+    };
+return (
+    <Spinner active={select.waiting}>
+        <CommentList
+            list={lists.comment}
+            exists={exists}
+            parent={selectParent}
+            onChangeParent={callbacks.changeSelectParent}
+            onAdd={callbacks.addComment}
+            onResetParent={callbacks.resetParent}
+            onSignIn={callbacks.onSignIn}>
+        </CommentList>
+        {selectParent._id===articleId && <AddCommentForm exists={exists} onSignIn={callbacks.onSignIn} onAdd={callbacks.addComment}></AddCommentForm>}
+    </Spinner>
+);
+};
+
+Comments.propTypes={
+    articleId:PropTypes.string
+}
+
+export default memo(Comments);
