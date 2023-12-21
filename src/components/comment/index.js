7dfbcd6cc6e5e28483a2bfa5edf6714900b5b731
AbdullahAlimov <ilia.filatov.04@gmail.com>
2023-12-21 23:42:08 +0300

    обновление компонента комментария

--- src/components/comment/index.js
+++ src/components/comment/index.js
@@ -1,15 +1,42 @@
-import React from 'react';
+import React, { memo } from 'react';
+import PropTypes from 'prop-types';
 import "./style.css"
 
-function Comment({name,date,content}){
+function Comment({ name, date, content, id, onChangeParent }) {
+    const dateOptions = {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+        hour: 'numeric',
+        minute: 'numeric',
+    };
+
+    const formattedDate = new Date(date).toLocaleDateString('ru-RU', dateOptions);
+    const year = new Date(date).getFullYear();
+
+    const finalFormattedDate = formattedDate.replace(` ${year} г.`, ` ${year}`);
+
     return (
-        <div className='Comment'>
-           <p className='Comment-name'>{name}</p>
-           <p className='Comment-date'>{date}</p>
-           <p className='Comment-content'>{content}</p>
-           <button className='Comment-button'>Ответить</button>
-        </div>
+            <div className='Comment'>
+                <p className='Comment-name'>{name}</p>
+                <p className='Comment-date'>{finalFormattedDate}</p>
+                <p className='Comment-text'>{content}</p>
+                <button className='Comment-button' onClick={()=>onChangeParent(id) }>Ответить</button>
+            </div>
     );
 };
 
-export default Comment;
+Comment.propTypes = {
+    name: PropTypes.string,
+    date: PropTypes.string,
+    content: PropTypes.string,
+    id: PropTypes.string,
+    onChangeParent: PropTypes.func
+}
+
+Comment.defaultProps={
+    onChangeParent:()=>{}
+}
+
+
+export default memo(Comment);
